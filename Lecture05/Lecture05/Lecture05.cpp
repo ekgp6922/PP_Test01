ffaaad4fa20f5319631980216815c20628b65440
Hanneoul <elflee77@gmail.com>
2023-04-03 11:45:17 +0900

    elflee77, gotoxy

--- Lecture05/Lecture05/Lecture05.cpp
+++ Lecture05/Lecture05/Lecture05.cpp
@@ -1,6 +1,7 @@
 ﻿//#define DEBUG
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
 
 // game_state == 0 일때
 int print_title_screen()
@@ -21,6 +22,24 @@ int print_title_screen()
     std::cout << "*   3. 게임 랭킹 보기                    *" << std::endl;
     std::cout << "*   4. 게임 종료 (esc)                   *" << std::endl;
     std::cout << "******************************************" << std::endl;
+
+    return 0;
+}
+
+void gotoxy(int x, int y) {
+    //x, y 좌표 설정
+    COORD pos = { x,y };
+    //커서 이동
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int print_game_screen(int stage_width, int stage_height)
+{
+    gotoxy(5, 5);
+
+    std::cout << "^^";
+    
+
 
     return 0;
 }
@@ -50,6 +69,7 @@ int main()
             switch(key_input)
             {
             case '1':
+                game_state = 1;
                 break;
             case '2':
                 game_state = 2;
@@ -66,6 +86,9 @@ int main()
                 break;
             }
             break;
+        case 1:
+            print_game_screen(10,10);
+            key_input = _getch();
         case 2:
             print_introduction_screen();
             key_input = _getch();
